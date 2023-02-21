46b889204db1bb90bea083a02eec0fbdadbfe8af
Kyuubang <bayhaqisptr04@gmail.com>
2023-02-21 12:34:19 +0700

    feat: integrate with login, score api

---
+++ cmd/login.go
@@ -0,0 +1,98 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/Kyuubang/philo/internal/api"
+	"github.com/Kyuubang/philo/logger"
+	"github.com/spf13/cobra"
+	"golang.org/x/term"
+	"os"
+	"strings"
+	"syscall"
+)
+
+func credentials() (string, string, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	// getting input user name
+	fmt.Print("Username: ")
+	username, err := reader.ReadString('\n')
+	if err != nil {
+		return "", "", err
+	}
+
+	// getting password input
+	fmt.Print("Password: ")
+	bytePassword, err := term.ReadPassword(syscall.Stdin)
+	if err != nil {
+		return "", "", err
+	}
+
+	password := string(bytePassword)
+	return strings.TrimSpace(username), strings.TrimSpace(password), nil
+}
+
+func (r Runner) login(host string) {
+	r.Log.MainLog.Info().Msg("Login")
+	logger.Console("Login to " + host).Start()
+
+	// get username and password
+	user, pass, _ := credentials()
+	fmt.Print("\n")
+
+	// login to custom server
+	if host != "github.com" {
+		cServer := api.CustomServer{
+			Host: host,
+		}
+
+		access, err := cServer.Login(user, pass)
+		if err != nil {
+			// check if error is ErrorLoginFailed
+			switch err {
+			case api.ErrorUserOrPass:
+				logger.Console("Username or password is incorrect").Error()
+				os.Exit(1)
+			case api.ErrorServer:
+				logger.Console("Server error").Error()
+				os.Exit(1)
+			default:
+				fmt.Println(err)
+				logger.Console("Unknown error").Error()
+			}
+		}
+
+		// write to config.json
+		authsData := map[string]string{
+			"host":     host,
+			"username": user,
+			"token":    access.Token,
+		}
+
+		r.Config.Set("auths", authsData)
+		err = r.Config.WriteConfig()
+		if err != nil {
+			logger.Console("Error writing config file").Error()
+			os.Exit(1)
+		}
+
+	}
+}
+
+func loginCommand() (cmd *cobra.Command) {
+	cmd = &cobra.Command{
+		Use:   "login [server]",
+		Short: "config is a command to manage config file",
+		Long:  `config is a command to manage config file`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				logger.Console("Missing server").Error()
+				os.Exit(1)
+			}
+			runner.login(args[0])
+		},
+	}
+
+	return
+}
